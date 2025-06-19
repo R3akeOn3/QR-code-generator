9276bff4dff457890d16d00cc76ded1c2d0b1061
R3akeOn3 <szymszyl357@gmail.com>
2025-06-19 18:14:32 +0200

    Create QR-code.py

---
+++ QR-code.py
@@ -0,0 +1,21 @@
+import qrcode
+from PIL import Image
+
+data = input("QR code link: ")
+filename  = input("Filename(default: qr_128x128): ")
+qrsize = input("size for your qr code: ")
+
+qr = qrcode.QRCode(
+    version=1,
+    box_size=10,
+    border=1
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
+img = img.resize((qrsize, qrsize), Image.NEAREST)
+
+template = (filename or "qr_128x128") + ".png"
+img.save(template)
+print("Created your QR code as: ", template)
